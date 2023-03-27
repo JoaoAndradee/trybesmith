a1cacbf17bc183ee517c480e3c01f66684ff55c6
joao <joao.dev94@gmail.com>
2023-03-27 20:42:06 -0300

    Requisito 07 - Validacao do campo usuario

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
 import userController from '../controllers/user.controller';
+import validateUser from '../middlewares/userValidation';
 
 const router = Router();
 
 router.post(
   '/',
+  validateUser.validateUsername,
+  validateUser.validateVocation,
+  validateUser.validateLevel,
+  validateUser.validatePassword,
   userController.addUser,
 );
 
